f055bce5b9b4836d3fa60c295ab4e0f6f9c5e2c5
smakaefa26522 <jsissnsusnaosushdhid@gmail.com>
2025-01-26 09:48:47 +0530

    Update api4.py

--- api4.py
+++ api4.py
@@ -44,7 +44,7 @@ def update_soul_txt(public_url):
 def update_vps_soul_txt(public_url):
     vps_ip = "147.93.30.18"
     vps_user = "root"
-    vps_password = "SoulCracks@9001"
+    vps_password = "SoulCracks@90011"
 
     try:
         ssh = paramiko.SSHClient()
